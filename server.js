e5cb1b17ac335f142ef9168c794d8bb3424f48e1
Antony <antony.vargasg@latinka.com.pe>
2026-04-08 11:31:50 -0500

    Vercelv2

--- server.js
+++ server.js
@@ -27,5 +27,15 @@ app.get('/api/dashboard/:cod', async (req, res) => {
     res.json(resultado);
 });
 
+
+// Agrega esto justo antes del const PORT = process.env.PORT...
+const path = require('path');
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor de desarrollo corriendo en http://localhost:${PORT}`));
+
+// Agrega esto al final de tu server.js
+module.exports = app;
