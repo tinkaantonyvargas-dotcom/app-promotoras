a05e1f6afa4987f5b2b639883dec4bf874c5f412
Juan Saco <juan.saco@latinka.com.pe>
2026-04-10 12:04:28 -0500

    cambios color

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ app.get('/api/dashboard/:cod', async (req, res) => {
 
 // Agrega esto justo antes del const PORT = process.env.PORT...
 const path = require('path');
+const { decode } = require('querystring');
 
 // Cambia esto en server.js
 // Ojo: /.*/ va SIN comillas simples ni dobles.
@@ -43,6 +44,3 @@ app.listen(PORT, () => console.log(`Servidor de desarrollo corriendo en http://l
 
 // Agrega esto al final de tu server.js
 module.exports = app;
-
-
-// Saludos JSA
